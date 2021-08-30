887e2c7b95b4146f548c56f3e78a92c27fa437c3
Riley Norris <rileydnorris@gmail.com>
2021-08-29 23:28:43 -0500

    Add new movement system

--- LocalMultiplayerGame/Assets/Scripts/Player/PlayerInputController.cs
+++ LocalMultiplayerGame/Assets/Scripts/Player/PlayerInputController.cs
@@ -10,10 +10,7 @@ public class PlayerInputController : MonoBehaviour
     private PlayerControls _controls;
     private CharacterController2D _characterController;
     private bool _isAiming = false;
-    private InputAction.CallbackContext _thumbstickContext;
-    public float gravity = -9.8f;
-    private Vector3 velocity = new Vector3();
-    public float groundDamping = 20f; // how fast do we change direction? higher means faster
+    private Vector2 _thumbstickMovement;
 
     void Awake()
     {
@@ -22,17 +19,23 @@ public class PlayerInputController : MonoBehaviour
         _aimController = GetComponentInChildren<AimController>();
         _characterController = GetComponent<CharacterController2D>();
 
-        _controls.Ground.Move.performed += context => _thumbstickContext = context;
-        // _controls.Ground.Move.canceled += context => _thumbstickContext = context;
-
-        // _controls.Ground.Jump.performed += context => _playerMovement.Jump();
+        _controls.Ground.Move.performed += context => _thumbstickMovement = context.ReadValue<Vector2>();
+        _controls.Ground.Move.canceled += context => _thumbstickMovement = context.ReadValue<Vector2>();
+        _controls.Ground.Jump.performed += context => _playerMovement.SetJumpValue(true);
+        _controls.Ground.Jump.canceled += context => _playerMovement.StopJump();
         _controls.Ground.Fire_Start.performed += context => TriggerPress(true);
         _controls.Ground.Fire_End.performed += context => TriggerPress(false);
     }
 
     void Update()
     {
-        ThumbstickMovement(_thumbstickContext);
+        if (_isAiming)
+        {
+            _aimController.UpdateArcPosition(_thumbstickMovement);
+            _playerMovement.SetThumbstickMovement(Vector2.zero);
+        }
+        else
+            _playerMovement.SetThumbstickMovement(_thumbstickMovement);
     }
 
     void OnEnable()
@@ -43,46 +46,6 @@ public class PlayerInputController : MonoBehaviour
     void OnDisable()
     {
         _controls.Ground.Disable();
-    }
-
-    void ThumbstickMovement(InputAction.CallbackContext context)
-    {
-        Debug.Log(context);
-        var oldVelocity = _characterController.velocity;
-        var movementValue = context.ReadValue<Vector2>();
-        var deadzone = 0.3f;
-
-        // This
-        // var velocity = _characterController.velocity;
-        float runSpeed = 5f;
-
-        // Debug.Log(movementValue);
-        // if (Mathf.Abs(movementValue.x) > deadzone || Mathf.Abs(movementValue.y) > deadzone)
-        // {
-            if (!_isAiming)
-            {
-                // Thumbstick moves player
-                // _playerMovement.UpdateMoveInput(movementValue);
-                // var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
-                velocity.x = Mathf.Lerp( velocity.x, -1 * runSpeed, Time.deltaTime * groundDamping );
-            }
-            else
-            {
-                // Thumbstick aims and stops player motion
-                _aimController.UpdateArcPosition(movementValue);
-                // _playerMovement.UpdateMoveInput(new Vector2(0, 0));
-            }
-        // }
-        // else
-        // {
-        //     // _playerMovement.UpdateMoveInput(new Vector2(0, 0));
-        //     velocity.x = 0;
-        // }
-
-        // velocity.y += gravity * Time.deltaTime;
-
-        _characterController.move(velocity * Time.deltaTime);
-        velocity = _characterController.velocity;
     }
 
     void TriggerPress(bool isAiming)
--- LocalMultiplayerGame/Assets/Scripts/Player/PlayerMovement.cs
+++ LocalMultiplayerGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,107 +1,143 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Prime31;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float movementSpeed;
-    public float jumpForce;
-    public float secondaryJumpForce;
-    private bool _canJump = true;
-    private Rigidbody2D _rigidBody;
     private Animator _animator;
-    private bool isFacingRight = true;
-    private Vector2 _moveInput;
-    private int _jumpCount = 0;
+    private ParticleSystem _dustTrail;
+    private ParticleSystem.VelocityOverLifetimeModule _dustTrailVelocity;
+    private ParticleSystem.EmissionModule _dustTrailEmission;
+    private CharacterController2D _characterController;
+    private Vector2 _thumbstickMovement = new Vector2();
+    private Vector3 velocity = new Vector3(0, 0, 0);
+    private float deadzone = 0.3f;
+    private bool _scheduledJump = false;
+    private float hangCounter = 0f;
+    private float jumpBufferCounter = 0f;
+    private float jumpDisableDelay = 0f;
+    private float jumpDisableTime = 0.2f;
+    public float gravity = -45f;
+    public float groundDamping = 20f;
+    public float jumpHeight = 3.5f;
+    public float runSpeed = 6.75f;
+    public float maxHangTime = 0.2f;
+    public float maxJumpBuffer = .1f;
 
-    private float distToGround;
-
-    void Start()
+    void Awake()
     {
-        _rigidBody = GetComponent<Rigidbody2D>();
+        _characterController = GetComponent<CharacterController2D>();
         _animator = GetComponent<Animator>();
-        distToGround = GetComponent<CircleCollider2D>().bounds.extents.y;
+        _dustTrail = GetComponentInChildren<ParticleSystem>();
+
+        _dustTrailVelocity = _dustTrail.velocityOverLifetime;
+        _dustTrailEmission = _dustTrail.emission;
     }
 
     void Update()
     {
-        // if (_rigidBody == null || _animator == null) return;
-        // HandleLanding();
-        // HandleMovement();
+        RotatePlayer();
+        HandleHangTime();
+        HandleJumpBuffer();
+
+        jumpDisableDelay -= Time.deltaTime;
+
+        velocity.x = GetMoveValue();
+        velocity.y = GetJumpValue();
+        velocity.y += gravity * Time.deltaTime;
+
+        _characterController.move(velocity * Time.deltaTime);
+        velocity = _characterController.velocity;
     }
 
-    void HandleLanding()
+    void RotatePlayer()
     {
-        // Debug.Log(distToGround);
-        if (Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.6f), -Vector3.up, 0.1f) && _rigidBody.velocity.y < 0)
+        transform.rotation = _thumbstickMovement.x < 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+        _dustTrailVelocity.x = _thumbstickMovement.x < 0 ? 0.2f : -0.2f;
+    }
+
+    void HandleHangTime()
+    {
+        if (_characterController.isGrounded)
+            hangCounter = maxHangTime;
+        else
+            hangCounter -= Time.deltaTime;
+    }
+
+    void HandleJumpBuffer()
+    {
+        // Start timer when user wants to jump
+        if (_scheduledJump)
+            jumpBufferCounter = maxJumpBuffer;
+        else
+            jumpBufferCounter -= Time.deltaTime;
+    }
+
+    bool CanJump()
+    {
+        // Only jump if the user:
+        // * Is touching the ground or in coyote time
+        // * Has requested to jump within the last 0.X seconds
+        // * Did not just start jumping
+        return hangCounter > 0 && jumpBufferCounter >= 0 && jumpDisableDelay < 0;
+    }
+
+    float GetJumpValue()
+    {
+        if (CanJump())
         {
-            _jumpCount = 0;
-            _animator.SetBool("IsJumping", false);
-            // Debug.Log("jump count" + _jumpCount);
+            jumpBufferCounter = 0;
+            jumpDisableDelay = jumpDisableTime;
+            _scheduledJump = false;
+            _animator.SetBool("IsJumping", true);
+            return Mathf.Sqrt(2f * jumpHeight * -gravity);
         }
-        // var startPos = new Vector3(transform.position.x, transform.position.y - 0.6f, 0);
-        // var distance = 0.1f;
-        // var endPos = startPos;
-        // endPos.y -= distance;
-        // Debug.DrawLine(startPos, endPos, Color.blue);
+
+        if (_characterController.isGrounded)
+            _animator.SetBool("IsJumping", false);
+
+        _scheduledJump = false;
+        return velocity.y;
+    }
+
+    float GetMoveValue()
+    {
+        _dustTrailEmission.rateOverTime = 0;
+        if (Mathf.Abs(_thumbstickMovement.x) > deadzone || Mathf.Abs(_thumbstickMovement.y) > deadzone)
+        {
+            if (_characterController.isGrounded)
+                _dustTrailEmission.rateOverTime = 3f;
+            var moveValue = Mathf.Lerp(velocity.x, _thumbstickMovement.x * runSpeed, Time.deltaTime * groundDamping);
+            _animator.SetFloat("Speed", moveValue);
+            return moveValue;
+        }
+        else
+        {
+            _animator.SetFloat("Speed", 0);
+            return 0;
+        }
+    }
+
+    public void SetThumbstickMovement(Vector2 moveValue)
+    {
+        _thumbstickMovement = moveValue;
+    }
+
+    public void SetJumpValue(bool doJump)
+    {
+        _scheduledJump = doJump;
+    }
+
+    public void StopJump()
+    {
+        if (velocity.y > 0)
+        {
+            velocity.y *= 0.5f;
+        }
     }
 
     public bool GetIsFacingRight()
     {
-        return isFacingRight;
-    }
-
-    private void HandleMovement()
-    {
-        var horizontalMovement = _moveInput.x;
-        var directionMultiplier = horizontalMovement < 0 ? -1.0f : 1.0f;
-        if (Mathf.Abs(horizontalMovement) > 0.15)
-        {
-            _rigidBody.velocity = new Vector3(movementSpeed * directionMultiplier, _rigidBody.velocity.y, 0);
-            _animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
-
-            // Rotate player by direction
-            transform.rotation = horizontalMovement < 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
-            isFacingRight = horizontalMovement > 0;
-        }
-        else
-        {
-            _rigidBody.velocity = new Vector3(0, _rigidBody.velocity.y, 0);
-            _animator.SetFloat("Speed", 0);
-        }
-    }
-
-    public void Jump()
-    {
-        if (_jumpCount == 0)
-        {
-            // Calculate the gravity and initial jump velocity values 
-            // _jumpGravity = -(2 * JumpHeight) / Mathf.Pow(TimeToJumpHeight, 2);
-            // _jumpVelocity = Mathf.Abs(_jumpGravity) * TimeToJumpHeight;
-
-            // // Step update
-            // stepMovement = (_velocity + Vector3.up * _gravity * Time.deltaTime * 0.5f) * Time.deltaTime;
-            // transform.Translate(stepMovement);
-            // _velocity.y += _gravity * Time.deltaTime;
-
-            // // When jump button pressed,
-            // _velocity.y = _jumpVelocity;
-
-            _rigidBody.AddForce(new Vector2(0, jumpForce));
-            _animator.SetBool("IsJumping", true);
-            _jumpCount += 1;
-        }
-        else if (_jumpCount == 1)
-        {
-            _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, 0);
-            _rigidBody.AddForce(new Vector2(0, secondaryJumpForce), ForceMode2D.Impulse);
-            _animator.SetBool("IsJumping", true);
-            _canJump = false;
-            _jumpCount += 1;
-        }
-    }
-
-    public void UpdateMoveInput(Vector2 moveInput)
-    {
-        _moveInput = moveInput;
+        return _thumbstickMovement.x > 0;
     }
 }
